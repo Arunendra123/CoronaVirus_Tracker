829bc6283332e94cdd146736c128ce8e8712a603
Arunendra Dubey <Arunendra.Dubey@microfocus.com>
2022-03-15 06:53:04 +0530

    Covid report tracker

---
+++ src/main/java/com/service/CoronaVirusDataService.java
@@ -0,0 +1,9 @@
+package com.service;
+
+public class CoronaVirusDataService {
+	
+	 private String CORONA_VIRUS_DATA_URL="https://github.com/CSSEGISandData/COVID-19/blob/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_US.csv";
+     public void fetchData() {
+    	 
+     }
+}
